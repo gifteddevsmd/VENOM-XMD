77a1a889d6e4f501b3188ee69db9912ae4d7faa5
giftdee <discordbotz526@gmail.com>
2025-08-21 14:08:34 +0300

    Update main.js

--- main.js
+++ main.js
@@ -204,8 +204,325 @@ async function startdave() {
   dave.ev.on('creds.update', saveCreds)
   dave.ev.on("messages.upsert", () => { })
 
-  return dave
+          
+
+
+
+
+      //autostatus view
+              dave.ev.on('messages.upsert', async chatUpdate => {
+        	if (global.statusview){
+        try {
+            if (!chatUpdate.messages || chatUpdate.messages.length === 0) return;
+            const mek = chatUpdate.messages[0];
+
+            if (!mek.message) return;
+            mek.message =
+                Object.keys(mek.message)[0] === 'ephemeralMessage'
+                    ? mek.message.ephemeralMessage.message
+                    : mek.message;
+
+            if (mek.key && mek.key.remoteJid === 'status@broadcast') {
+                let emoji = [ "💙","❤️", "🌚","😍", "😭" ];
+                let sigma = emoji[Math.floor(Math.random() * emoji.length)];
+                await dave.readMessages([mek.key]);
+                dave.sendMessage(
+                    'status@broadcast',
+                    { react: { text: sigma, key: mek.key } },
+                    { statusJidList: [mek.key.participant] },
+                );
+            }
+
+        } catch (err) {
+            console.error(err);
+        }
+      }
+   }
+ )  
+    
+dave.ev.on('group-participants.update', async (anu) => {
+    const iswel = db.data.chats[anu.id]?.welcome || false
+    const isLeft = db.data.chats[anu.id]?.goodbye || false
+
+    let {
+      welcome
+    } = require('./library/lib/welcome')
+    await welcome(iswel, isLeft, dave, anu)
+  })
+            
+    dave.ev.on('messages.upsert', async chatUpdate => {
+        //console.log(JSON.stringify(chatUpdate, undefined, 2))
+        try {
+            mek = chatUpdate.messages[0]
+            if (!mek.message) return
+            mek.message = (Object.keys(mek.message)[0] === 'ephemeralMessage') ? mek.message.ephemeralMessage.message : mek.message
+            if (mek.key && mek.key.remoteJid === 'status@broadcast') return
+            if (!dave.public && !mek.key.fromMe && chatUpdate.type === 'notify') return
+            if (mek.key.id.startsWith('Xeon') && mek.key.id.length === 16) return
+            if (mek.key.id.startsWith('BAE5')) return
+            m = smsg(dave, mek, store)
+            require("./davehandler")(dave, m, chatUpdate, store)
+        } catch (err) {
+            console.log(err)
+        }
+    })
+
+   
+    dave.decodeJid = (jid) => {
+        if (!jid) return jid
+        if (/:\d+@/gi.test(jid)) {
+            let decode = jidDecode(jid) || {}
+            return decode.user && decode.server && decode.user + '@' + decode.server || jid
+        } else return jid
+    }
+
+    dave.ev.on('contacts.update', update => {
+        for (let contact of update) {
+            let id = dave.decodeJid(contact.id)
+            if (store && store.contacts) store.contacts[id] = {
+                id,
+                name: contact.notify
+            }
+        }
+    })
+
+    dave.getName = (jid, withoutContact = false) => {
+        id = dave.decodeJid(jid)
+        withoutContact = dave.withoutContact || withoutContact
+        let v
+        if (id.endsWith("@g.us")) return new Promise(async (resolve) => {
+            v = store.contacts[id] || {}
+            if (!(v.name || v.subject)) v = dave.groupMetadata(id) || {}
+            resolve(v.name || v.subject || PhoneNumber('+' + id.replace('@s.whatsapp.net', '')).getNumber('international'))
+        })
+        else v = id === '0@s.whatsapp.net' ? {
+                id,
+                name: 'WhatsApp'
+            } : id === dave.decodeJid(dave.user.id) ?
+            dave.user :
+            (store.contacts[id] || {})
+        return (withoutContact ? '' : v.name) || v.subject || v.verifiedName || PhoneNumber('+' + jid.replace('@s.whatsapp.net', '')).getNumber('international')
+    }
+
+dave.sendContact = async (jid, kon, quoted = '', opts = {}) => {
+	let list = []
+	for (let i of kon) {
+	    list.push({
+	    	displayName: await dave.getName(i),
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await dave.getName(i)}\nFN:${await dave.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Mobile\nEND:VCARD`
+	    })
+	}
+	dave.sendMessage(jid, { contacts: { displayName: `${list.length} Contact`, contacts: list }, ...opts }, { quoted })
+    }
+
+    dave.public = true
+
+    dave.serializeM = (m) => smsg(dave, m, store)
+
+    dave.sendText = (jid, text, quoted = '', options) => dave.sendMessage(jid, {
+        text: text,
+        ...options
+    }, {
+        quoted,
+        ...options
+    })
+    dave.sendImage = async (jid, path, caption = '', quoted = '', options) => {
+        let buffer = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,` [1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+        return await dave.sendMessage(jid, {
+            image: buffer,
+            caption: caption,
+            ...options
+        }, {
+            quoted
+        })
+    }
+    dave.sendTextWithMentions = async (jid, text, quoted, options = {}) => dave.sendMessage(jid, {
+        text: text,
+        mentions: [...text.matchAll(/@(\d{0,16})/g)].map(v => v[1] + '@s.whatsapp.net'),
+        ...options
+    }, {
+        quoted
+    })
+    dave.sendImageAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifImg(buff, options)
+} else {
+buffer = await imageToWebp(buff)
 }
+await dave.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+.then( response => {
+fs.unlinkSync(buffer)
+return response
+})
+}
+
+dave.sendVideoAsSticker = async (jid, path, quoted, options = {}) => {
+let buff = Buffer.isBuffer(path) ? path : /^data:.*?\/.*?;base64,/i.test(path) ? Buffer.from(path.split`,`[1], 'base64') : /^https?:\/\//.test(path) ? await (await getBuffer(path)) : fs.existsSync(path) ? fs.readFileSync(path) : Buffer.alloc(0)
+let buffer
+if (options && (options.packname || options.author)) {
+buffer = await writeExifVid(buff, options)
+} else {
+buffer = await videoToWebp(buff)
+}
+await dave.sendMessage(jid, { sticker: { url: buffer }, ...options }, { quoted })
+return buffer
+}
+    dave.downloadAndSaveMediaMessage = async (message, filename, attachExtension = true) => {
+        let quoted = message.msg ? message.msg : message
+        let mime = (message.msg || message).mimetype || ''
+        let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+        const stream = await downloadContentFromMessage(quoted, messageType)
+        let buffer = Buffer.from([])
+        for await (const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk])
+        }
+        let type = await FileType.fromBuffer(buffer)
+        trueFileName = attachExtension ? (filename + '.' + type.ext) : filename
+        // save to file
+        await fs.writeFileSync(trueFileName, buffer)
+        return trueFileName
+    }
+const storeFile = "./library/database/store.json";
+const maxMessageAge = 24 * 60 * 60; //24 hours
+
+function loadStoredMessages() {
+    if (fs.existsSync(storeFile)) {
+        try {
+            return JSON.parse(fs.readFileSync(storeFile));
+        } catch (err) {
+            console.error("⚠️ Error loading store.json:", err);
+            return {};
+        }
+    }
+    return {};
+}
+
+function saveStoredMessages(chatId, messageId, messageData) {
+    let storedMessages = loadStoredMessages();
+
+    if (!storedMessages[chatId]) storedMessages[chatId] = {};
+    if (!storedMessages[chatId][messageId]) {
+        storedMessages[chatId][messageId] = messageData;
+        fs.writeFileSync(storeFile, JSON.stringify(storedMessages, null, 2));
+    }
+} 
+
+function cleanupOldMessages() {
+    let now = Math.floor(Date.now() / 1000);
+    let storedMessages = {};
+
+    if (fs.existsSync(storeFile)) {
+        try {
+            storedMessages = JSON.parse(fs.readFileSync(storeFile));
+        } catch (err) {
+            console.error("❌ Error reading store.json:", err);
+            return;
+        }
+    }
+
+    let totalMessages = 0, oldMessages = 0, keptMessages = 0;
+
+    for (let chatId in storedMessages) {
+        let messages = storedMessages[chatId];
+
+        for (let messageId in messages) {
+            let messageTimestamp = messages[messageId].timestamp;
+
+            if (typeof messageTimestamp === "object" && messageTimestamp.low !== undefined) {
+                messageTimestamp = messageTimestamp.low;
+            }
+
+            if (messageTimestamp > 1e12) {
+                messageTimestamp = Math.floor(messageTimestamp / 1000);
+            }
+
+            totalMessages++;
+
+            if (now - messageTimestamp > maxMessageAge) {
+                delete storedMessages[chatId][messageId];
+                oldMessages++;
+            } else {
+                keptMessages++;
+            }
+        }
+        
+        if (Object.keys(storedMessages[chatId]).length === 0) {
+            delete storedMessages[chatId];
+        }
+    }
+
+    fs.writeFileSync(storeFile, JSON.stringify(storedMessages, null, 2));
+
+    console.log("[RACHEL-XMD] 🧹 Cleaning up:");
+    console.log(`- Total messages processed: ${totalMessages}`);
+    console.log(`- Old messages removed: ${oldMessages}`);
+    console.log(`- Remaining messages: ${keptMessages}`);
+}
+dave.ev.on("messages.upsert", async (chatUpdate) => {
+    for (const msg of chatUpdate.messages) {
+        if (!msg.message) return;
+
+        let chatId = msg.key.remoteJid;
+        let messageId = msg.key.id;
+
+        saveStoredMessages(chatId, messageId, msg);
+    }
+});
+    dave.copyNForward = async (jid, message, forceForward = false, options = {}) => {
+let vtype
+if (options.readViewOnce) {
+message.message = message.message && message.message.ephemeralMessage && message.message.ephemeralMessage.message ? message.message.ephemeralMessage.message : (message.message || undefined)
+vtype = Object.keys(message.message.viewOnceMessage.message)[0]
+delete(message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
+delete message.message.viewOnceMessage.message[vtype].viewOnce
+message.message = {
+...message.message.viewOnceMessage.message
+}
+}
+let mtype = Object.keys(message.message)[0]
+let content = await generateForwardMessageContent(message, forceForward)
+let ctype = Object.keys(content)[0]
+let context = {}
+if (mtype != "conversation") context = message.message[mtype].contextInfo
+content[ctype].contextInfo = {
+...context,
+...content[ctype].contextInfo
+}
+const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+...content[ctype],
+...options,
+...(options.contextInfo ? {
+contextInfo: {
+...content[ctype].contextInfo,
+...options.contextInfo
+}
+} : {})
+} : {})
+await dave.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
+return waMessage
+}
+    
+    dave.sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return dave.sendMessage(jid, { poll: { name, values, selectableCount }}) }
+
+dave.parseMention = (text = '') => {
+return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
+}
+            
+    dave.downloadMediaMessage = async (message) => {
+        let mime = (message.msg || message).mimetype || ''
+        let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+        const stream = await downloadContentFromMessage(message, messageType)
+        let buffer = Buffer.from([])
+        for await (const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk])
+        }
+
+        return buffer
+    }
+    return dave
+}  
 
 async function tylor() {
   if (fs.existsSync(credsPath)) {
