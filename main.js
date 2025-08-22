195f650d024634222f01a82048683545bf7b2d67
giftdee <discordbotz526@gmail.com>
2025-08-23 01:15:27 +0300

    dave

--- main.js
+++ main.js
@@ -13,11 +13,10 @@ const path = require('path')
 const axios = require('axios')
 const _ = require('lodash')
 const moment = require('moment-timezone')
-const FileType = require('file-type');
 const PhoneNumber = require('awesome-phonenumber')
 const { imageToWebp, videoToWebp, writeExifImg, writeExifVid } = require('./library/lib/exif')
 const { smsg, isUrl, generateMessageTag, getBuffer, getSizeMedia, fetch, await, sleep, reSize } = require('./library/lib/function')
-const { default: daveConnect, getAggregateVotesInPollMessage, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@whiskeysockets/baileys")
+const { default: trashcoreConnect, getAggregateVotesInPollMessage, delay, PHONENUMBER_MCC, makeCacheableSignalKeyStore, useMultiFileAuthState, DisconnectReason, fetchLatestBaileysVersion, generateForwardMessageContent, prepareWAMessageMedia, generateWAMessageFromContent, generateMessageID, downloadContentFromMessage, makeInMemoryStore, jidDecode, proto } = require("@whiskeysockets/baileys")
 const channelId = "120363400480173280@newsletter";
 const createToxxicStore = require('./library/database/basestore');
 const store = createToxxicStore('./store', {
@@ -57,25 +56,7 @@ const useMobile = process.argv.includes("--mobile")
 
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout })
 const question = (text) => new Promise((resolve) => rl.question(text, resolve))
-const welcomeMessage = `
-💠════════════════════💠
-   💠 Created by Dave 💠
-💠════════════════════💠
 
-     👋 Hi, I Am ${global.botname}  
-     💠 Version   : 1.5.0  
-     💠 Owner     : ${global.owner}  
-     💠 Library   : WBaileys MD  
-     💠 Status    : Online  
-     💠 Session   : ${global.session}  
-
-     💠 Base By   : Gifted Dave 
-
-💠════════════════════💠
-   💠 Powered by Dave 💠
-💠════════════════════💠
-`;
-    console.log(welcomeMessage);  
 const sessionDir = path.join(__dirname, 'session');
 const credsPath = path.join(sessionDir, 'creds.json');
 
@@ -89,13 +70,13 @@ async function downloadSessionData() {
         return console.log(color(`Session id not found at SESSION_ID!\nCreds.json not found at session folder!\n\nWait to enter your number`, 'red'));
       }
 
-      const base64Data = global.SESSION_ID.split("DAVE-XMD-WHATSAPP-BOT;;;=>")[1];
-
+      const base64Data = global.SESSION_ID.split("dave~")[1];
+      
       const sessionData = Buffer.from(base64Data, 'base64');
-
+      
         await fs.promises.writeFile(credsPath, sessionData);
       console.log(color(`Session successfully saved, please wait!!`, 'green'));
-      await startdave();
+      await starttrashcore();
     }
   } catch (error) {
     console.error('Error downloading session data:', error);
@@ -103,12 +84,11 @@ async function downloadSessionData() {
 }
 
 
-async function startdave() {
+async function starttrashcore() {
 let { version, isLatest } = await fetchLatestBaileysVersion()
 const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
     const msgRetryCounterCache = new NodeCache() // for retry message, "waiting message"
     const dave = makeWASocket({
-        version: [2, 3000, 1023223821],
         logger: pino({ level: 'silent' }),
         printQRInTerminal: !pairingCode, // popping up QR in terminal log
       mobile: useMobile, // mobile api (prone to bans)
@@ -128,95 +108,112 @@ const {  state, saveCreds } =await useMultiFileAuthState(`./session`)
       msgRetryCounterCache, // Resolve waiting messages
       defaultQueryTimeoutMs: undefined, // for this issues https://github.com/WhiskeySockets/Baileys/issues/276
    })
-
+   
    store.bind(dave.ev)
 
     // login use pairing code
    // source code https://github.com/WhiskeySockets/Baileys/blob/master/Example/example.ts#L61
-        if (global.connect && !dave.authState.creds.registered) {
-        try {
-            const phoneNumber = await question(chalk.cyan(`\n[ ᯤ ] dave (--||--) Enter Your Number:\n`));
-            const code = await dave.requestPairingCode(phoneNumber.trim());
-            console.log(chalk.green(`\n[ ᯤ ] dave (--||--) Pairing Code:\n`), code);
-        } catch (error) {
-            console.error(chalk.red(`\nError during pairing:`), error.message);
-            return;
-        }
-    }
-    store?.bind(dave.ev)
+   if (pairingCode && !dave.authState.creds.registered) {
+      if (useMobile) throw new Error('Cannot use pairing code with mobile api')
+
+      let phoneNumber
+      if (!!phoneNumber) {
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +254xxx")))
+            process.exit(0)
+         }
+      } else {
+         phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number \nFor example: +254xxx : `)))
+         phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+
+         // Ask again when entering the wrong number
+         if (!Object.keys(PHONENUMBER_MCC).some(v => phoneNumber.startsWith(v))) {
+            console.log(chalk.bgBlack(chalk.redBright("Start with country code of your WhatsApp Number, Example : +254xxx")))
+
+            phoneNumber = await question(chalk.bgBlack(chalk.greenBright(`Please type your WhatsApp number \nFor example: +254xxx : `)))
+            phoneNumber = phoneNumber.replace(/[^0-9]/g, '')
+            rl.close()
+         }
+      }
+
+      setTimeout(async () => {
+         let code = await dave.requestPairingCode(phoneNumber)
+         code = code?.match(/.{1,4}/g)?.join("-") || code
+         console.log(chalk.black(chalk.bgGreen(`Your Pairing Code : `)), chalk.black(chalk.white(code)))
+      }, 3000)
+   }
+
 dave.ev.on('connection.update', async (update) => {
-        const {
-
-                connection,
-                lastDisconnect
-        } = update
+	const {
+        
+		connection,
+		lastDisconnect
+	} = update
 try{
-                if (connection === 'close') {
-                        let reason = new Boom(lastDisconnect?.error)?.output.statusCode
-                        if (reason === DisconnectReason.badSession) {
-                                console.log(`Bad Session File, Please Delete Session and Scan Again`);
-                                startdave()
-                        } else if (reason === DisconnectReason.connectionClosed) {
-                                console.log("Connection closed, reconnecting....");
-                                startdave();
-                        } else if (reason === DisconnectReason.connectionLost) {
-                                console.log("Connection Lost from Server, reconnecting...");
-                                startdave();
-                        } else if (reason === DisconnectReason.connectionReplaced) {
-                                console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
-                                startdave()
-                        } else if (reason === DisconnectReason.loggedOut) {
-                                console.log(`Device Logged Out, Please Delete Session and Scan Again.`);
-                                startdave();
-                        } else if (reason === DisconnectReason.restartRequired) {
-                                console.log("Restart Required, Restarting...");
-                                startdave();
-                        } else if (reason === DisconnectReason.timedOut) {
-                                console.log("Connection TimedOut, Reconnecting...");
-                                startdave();
-                        } else dave.end(`Unknown DisconnectReason: ${reason}|${connection}`)
-                }
-                if (update.connection == "connecting" || update.receivedPendingNotifications == "false") {
-                        console.log(color(`\nConnecting...`, 'white'))
-                }
-                if (update.connection == "open" || update.receivedPendingNotifications == "true") {
-                        console.log(color(` `,'magenta'))
+		if (connection === 'close') {
+			let reason = new Boom(lastDisconnect?.error)?.output.statusCode
+			if (reason === DisconnectReason.badSession) {
+				console.log(`Bad Session File, Please Delete Session and Scan Again`);
+				starttrashcore()
+			} else if (reason === DisconnectReason.connectionClosed) {
+				console.log("Connection closed, reconnecting....");
+				starttrashcore();
+			} else if (reason === DisconnectReason.connectionLost) {
+				console.log("Connection Lost from Server, reconnecting...");
+				starttrashcore();
+			} else if (reason === DisconnectReason.connectionReplaced) {
+				console.log("Connection Replaced, Another New Session Opened, Please Close Current Session First");
+				starttrashcore()
+			} else if (reason === DisconnectReason.loggedOut) {
+				console.log(`Device Logged Out, Please Delete Session and Scan Again.`);
+				starttrashcore();
+			} else if (reason === DisconnectReason.restartRequired) {
+				console.log("Restart Required, Restarting...");
+				starttrashcore();
+			} else if (reason === DisconnectReason.timedOut) {
+				console.log("Connection TimedOut, Reconnecting...");
+				starttrashcore();
+			} else dave.end(`Unknown DisconnectReason: ${reason}|${connection}`)
+		}
+		if (update.connection == "connecting" || update.receivedPendingNotifications == "false") {
+			console.log(color(`\nConnecting...`, 'white'))
+		}
+		if (update.connection == "open" || update.receivedPendingNotifications == "true") {
+			console.log(color(` `,'magenta'))
             console.log(color(`Connected to => ` + JSON.stringify(dave.user, null, 2), 'green'))
-                        await delay(1999)
-                        dave.sendMessage(dave.user.id, {
+			await delay(1999)
+			dave.sendMessage(dave.user.id, {
 image: {
-url: 'https://files.catbox.moe/eh7hlm.jpg'
+url: 'https://url.bwmxmd.online/Adams.jin9796u.jpg'
 }, 
-caption: `
-💠════════════════════💠
-       💠 Created by Dave 💠
-💠════════════════════💠
+caption: ` [ ༑📚𝑪𝒓𝒆𝒂𝒕𝒆𝒅 𝒃𝒚 Dave ⿻ ༑]]
+┏─•⛩️ ${global.botname} ⛩️•─⬣[⿻
 
-👋 Hii, I Am ${global.botname}  
-💠 Version   : ${global.botversion}  
-💠 Owner     : ${global.owner}  
-💠 Library   : WBaileys MD  
-💠 Status    : Online  
-💠 Session   : ${global.session}  
+👋 Hii, I Am ${global.botname}
+ [⿻] 🌌 Version      : 1.3.0
+ [⿻] 👤 Owner  	     : ${global.owner}
+ [⿻] 📚 Library      : WBaileys MD
+ [⿻] 📱 Status       : Online
+ [⿻] 📝 Session     :  ${global.session}
+ 
+ [⿻] 🌎 Base By    : trashcoredevs
 
-💠 Base By   : davedevs  
-
-💠════════════════════💠
-       💠 Powered by Dave 💠
-💠════════════════════💠
-`
+┗─•${global.botname}•─⬣[⿻
+[[ ༑📚𝑪𝒓𝒆𝒂𝒕𝒆 𝑩𝒚 𝒕𝒓𝒂𝒔𝒉𝒄𝒐𝒓𝒆༢⿻ ༑]]`
 })
 
 
+			
 
-
-            console.log(color('>VENOM-XMD is Connected< [ ! ]','red'))
-                }
-
+            console.log('>VENOM -XMD is Connected< [ ! ]')
+		}
+	
 } catch (err) {
-          console.log('Error in Connection.update '+err)
-          startdave();
-        }
+	  console.log('Error in Connection.update '+err)
+	  starttrashcore();
+	}
 })
 dave.ev.on('creds.update', saveCreds)
 dave.ev.on("messages.upsert",  () => { })
@@ -224,14 +221,14 @@ dave.ev.on("messages.upsert",  () => { })
 
 
 
+	            
 
 
 
-
-
+    
     //autostatus view
               dave.ev.on('messages.upsert', async chatUpdate => {
-                if (global.statusview){
+        	if (global.statusview){
         try {
             if (!chatUpdate.messages || chatUpdate.messages.length === 0) return;
             const mek = chatUpdate.messages[0];
@@ -259,17 +256,9 @@ dave.ev.on("messages.upsert",  () => { })
       }
    }
  )  
+    
 
-dave.ev.on('group-participants.update', async (anu) => {
-    const iswel = db.data.chats[anu.id]?.welcome || false
-    const isLeft = db.data.chats[anu.id]?.goodbye || false
-
-    let {
-      welcome
-    } = require('./library/lib/welcome')
-    await welcome(iswel, isLeft, dave, anu)
-  })
-
+            
     dave.ev.on('messages.upsert', async chatUpdate => {
         //console.log(JSON.stringify(chatUpdate, undefined, 2))
         try {
@@ -281,13 +270,13 @@ dave.ev.on('group-participants.update', async (anu) => {
             if (mek.key.id.startsWith('Xeon') && mek.key.id.length === 16) return
             if (mek.key.id.startsWith('BAE5')) return
             m = smsg(dave, mek, store)
-            require("./davehandler")(dave, m, chatUpdate, store)
+            require("./trashhandler")(dave, m, chatUpdate, store)
         } catch (err) {
             console.log(err)
         }
     })
 
-
+   
     dave.decodeJid = (jid) => {
         if (!jid) return jid
         if (/:\d+@/gi.test(jid)) {
@@ -325,14 +314,14 @@ dave.ev.on('group-participants.update', async (anu) => {
     }
 
 dave.sendContact = async (jid, kon, quoted = '', opts = {}) => {
-        let list = []
-        for (let i of kon) {
-            list.push({
-                    displayName: await dave.getName(i),
-                    vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await dave.getName(i)}\nFN:${await dave.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Mobile\nEND:VCARD`
-            })
-        }
-        dave.sendMessage(jid, { contacts: { displayName: `${list.length} Contact`, contacts: list }, ...opts }, { quoted })
+	let list = []
+	for (let i of kon) {
+	    list.push({
+	    	displayName: await dave.getName(i),
+	    	vcard: `BEGIN:VCARD\nVERSION:3.0\nN:${await dave.getName(i)}\nFN:${await dave.getName(i)}\nitem1.TEL;waid=${i.split('@')[0]}:${i.split('@')[0]}\nitem1.X-ABLabel:Mobile\nEND:VCARD`
+	    })
+	}
+	dave.sendMessage(jid, { contacts: { displayName: `${list.length} Contact`, contacts: list }, ...opts }, { quoted })
     }
 
     dave.public = true
@@ -467,7 +456,7 @@ function cleanupOldMessages() {
                 keptMessages++;
             }
         }
-
+        
         if (Object.keys(storedMessages[chatId]).length === 0) {
             delete storedMessages[chatId];
         }
@@ -475,7 +464,7 @@ function cleanupOldMessages() {
 
     fs.writeFileSync(storeFile, JSON.stringify(storedMessages, null, 2));
 
-    console.log("[VENOM-XMD] 🧹 Cleaning up:");
+    console.log("[VENOM -XMD] 🧹 Cleaning up:");
     console.log(`- Total messages processed: ${totalMessages}`);
     console.log(`- Old messages removed: ${oldMessages}`);
     console.log(`- Remaining messages: ${keptMessages}`);
@@ -498,4 +487,83 @@ vtype = Object.keys(message.message.viewOnceMessage.message)[0]
 delete(message.message && message.message.ignore ? message.message.ignore : (message.message || undefined))
 delete message.message.viewOnceMessage.message[vtype].viewOnce
 message.message = {
-...message.message.
+...message.message.viewOnceMessage.message
+}
+}
+let mtype = Object.keys(message.message)[0]
+let content = await generateForwardMessageContent(message, forceForward)
+let ctype = Object.keys(content)[0]
+let context = {}
+if (mtype != "conversation") context = message.message[mtype].contextInfo
+content[ctype].contextInfo = {
+...context,
+...content[ctype].contextInfo
+}
+const waMessage = await generateWAMessageFromContent(jid, content, options ? {
+...content[ctype],
+...options,
+...(options.contextInfo ? {
+contextInfo: {
+...content[ctype].contextInfo,
+...options.contextInfo
+}
+} : {})
+} : {})
+await dave.relayMessage(jid, waMessage.message, { messageId:  waMessage.key.id })
+return waMessage
+}
+    
+    dave.sendPoll = (jid, name = '', values = [], selectableCount = 1) => { return dave.sendMessage(jid, { poll: { name, values, selectableCount }}) }
+
+dave.parseMention = (text = '') => {
+return [...text.matchAll(/@([0-9]{5,16}|0)/g)].map(v => v[1] + '@s.whatsapp.net')
+}
+            
+    dave.downloadMediaMessage = async (message) => {
+        let mime = (message.msg || message).mimetype || ''
+        let messageType = message.mtype ? message.mtype.replace(/Message/gi, '') : mime.split('/')[0]
+        const stream = await downloadContentFromMessage(message, messageType)
+        let buffer = Buffer.from([])
+        for await (const chunk of stream) {
+            buffer = Buffer.concat([buffer, chunk])
+        }
+
+        return buffer
+    }
+    return dave
+}
+
+async function tylor() {
+    if (fs.existsSync(credsPath)) {
+        console.log(color("Session file found, starting bot...", 'yellow'));
+await starttrashcore();
+} else {
+         const sessionDownloaded = await downloadSessionData();
+        if (sessionDownloaded) {
+            console.log("Session downloaded, starting bot.");
+await starttrashcore();
+    } else {
+     if (!fs.existsSync(credsPath)) {
+    if(!global.SESSION_ID) {
+            console.log(color("Please wait for a few seconds to enter your number!", 'red'));
+await starttrashcore();
+        }
+    }
+  }
+ }
+}
+
+tylor()
+
+process.on('uncaughtException', function (err) {
+let e = String(err)
+if (e.includes("conflict")) return
+if (e.includes("Socket connection timeout")) return
+if (e.includes("not-authorized")) return
+if (e.includes("already-exists")) return
+if (e.includes("rate-overlimit")) return
+if (e.includes("Connection Closed")) return
+if (e.includes("Timed Out")) return
+if (e.includes("Value not found")) return
+console.log('Caught exception: ', err)
+})
